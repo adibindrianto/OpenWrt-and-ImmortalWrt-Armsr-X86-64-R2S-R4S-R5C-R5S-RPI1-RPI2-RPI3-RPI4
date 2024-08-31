438d21bb69514c2526a4868ded44ef000e2a0e9f
Adi B Indrianto <133480318+adibindrianto@users.noreply.github.com>
2024-09-01 04:08:10 +0700

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -15,7 +15,7 @@ sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
 
 # Increase rootfs partition size to 500M
-sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=102/" .config
+sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=1024/" .config
 
 # Change default IP address from 192.168.1.1 to 100.100.100.1
 sed -i "s/192.168.1.1/100.100.100.1/g" package/base-files/files/etc/config/network

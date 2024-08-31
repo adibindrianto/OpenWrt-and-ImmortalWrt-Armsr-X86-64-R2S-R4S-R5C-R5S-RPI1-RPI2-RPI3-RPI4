05aa5a74be1a7b9412b34d1a7cc6773c275e142a
Adi B Indrianto <133480318+adibindrianto@users.noreply.github.com>
2024-09-01 02:57:12 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@ PROFILE="friendlyarm_nanopi-r2s"
 
 # Define packages
 PACKAGES="
-base-files busybox ca-bundle dnsmasq dropbear e2fsprogs firewall4 fstools kmod-gpio-button-hotplug kmod-nft-offload kmod-usb-net-rtl8152 libc libgcc libustream-mbedtls logd luci mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only opkg partx-utils ppp ppp-mod-pppoe procd procd-seccomp procd-ujail uboot-envtools uci uclient-fetch urandom-seed urngd
-
 # USB drivers
 kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb3 kmod-usb-net kmod-usb-net-asix kmod-usb-net-rtl8152 kmod-usb-net-rtl8153 kmod-usb-net-cdc-ether kmod-usb-net-cdc-mbim kmod-usb-net-cdc-ncm kmod-usb-net-hso kmod-usb-net-qmi-wwan kmod-usb-net-sieral kmod-usb-net-sierra kmod-usb-net-wwan kmod-usb-serial kmod-usb-serial-omni kmod-usb-serial-xt kmod-usb-serial-qualcomm
 

15e0ae8278a85ebbd409d39db078cbf57c392d48
Adi B Indrianto <133480318+adibindrianto@users.noreply.github.com>
2024-09-01 02:05:05 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ PROFILE="friendlyarm_nanopi-r2s"
 PACKAGES=""
 
 # Basic configuration for NanoPi R2S with OpenWrt 23.05.4
+
 # USB drivers
 PACKAGES="$PACKAGES kmod-usb-core kmod-usb-ohci kmod-usb2 kmod-usb3"
 
@@ -20,7 +21,7 @@ PACKAGES="$PACKAGES kmod-usb-net kmod-usb-net-asix kmod-usb-net-rtl8152"
 PACKAGES="$PACKAGES kmod-mt7601u kmod-rtl8812au-ct kmod-ath9k-htc kmod-rt2800-usb"
 
 # Basic OpenWrt packages
-PACKAGES="$PACKAGES luci luci-ssl luci-app-opkg"
+PACKAGES="$PACKAGES luci luci-ssl luci-app-opkg luci-app-firewall luci-base luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp"
 
 # Luci Bootstrap theme
 PACKAGES="$PACKAGES luci-theme-bootstrap"
@@ -33,6 +34,13 @@ PACKAGES="$PACKAGES iptables-mod-extra iptables-mod-tproxy"
 
 # Essential utilities
 PACKAGES="$PACKAGES openssh-server openssh-client"
+
+# Additional packages
+PACKAGES="$PACKAGES cgi-io libiwinfo libiwinfo-data libiwinfo-lua liblua liblucihttp liblucihttp-lua libubus-lua lua px5g-wolfssl rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci rpcd-mod-rrdns uhttpd uhttpd-mod-ubus luci-compat ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant"
+
+# Default OpenWrt packages
+# Add default packages installed by official OpenWrt
+PACKAGES="$PACKAGES base-files busybox dnsmasq dropbear firewall fstools jshn libblobmsg-json libiwinfo-lua libjson-c liblua libubox-lua libubus-lua logd mtd netifd odhcp6c odhcpd-base procd uci uclient-fetch uhttpd zlib"
 
 # Define the files directory for custom configurations or files
 FILES="files"

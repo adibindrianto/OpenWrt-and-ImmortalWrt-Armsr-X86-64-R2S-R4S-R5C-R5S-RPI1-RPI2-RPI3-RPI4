2dcca741919d0a5005367ffee7e828ea7249fabe
Adi B Indrianto <133480318+adibindrianto@users.noreply.github.com>
2024-09-01 10:15:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,9 +24,7 @@ kmod-usb-net-asix kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8150 kmod-usb-net-rt
 kmod-fs-ext4 kmod-fs-vfat kmod-fs-xfs kmod-fs-btrfs kmod-fs-f2fs kmod-fs-squashfs \
 kmod-fs-ntfs kmod-fs-ntfs3 kmod-usb-storage kmod-usb-storage-extras \
 kmod-nft-fib kmod-nft-bridge kmod-nft-nat \
-block-mount e2fsprogs fdisk kmod-fs-ext4 kmod-fs-vfat kmod-fs-ntfs kmod-fs-btrfs kmod-fs-xfs \
-kmod-fs-squashfs kmod-fs-f2fs \
-parted"
+blockd block-mount e2fsprogs fdisk parted blkid kmod-fs-cifs kmod-fs-exfat ntfs-3g ntfs-3g-utils gdisk tune2fs resize2fs losetup cfdisk f2fs-tools kmod-usb-storage-uas kmod-usb-ledtrig-usbport"
 
 # Files directory for custom configurations or files
 FILES="files"

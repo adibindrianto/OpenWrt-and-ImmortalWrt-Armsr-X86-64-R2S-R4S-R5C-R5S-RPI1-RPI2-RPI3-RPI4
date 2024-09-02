95d149c3ce0023ccc5c3d5d0fca742ac095753f4
Adi B Indrianto <133480318+adibindrianto@users.noreply.github.com>
2024-09-02 18:38:48 +0700

    Update builder-patch-rpi1.sh

--- scripts/builder-patch-rpi1.sh
+++ scripts/builder-patch-rpi1.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# Pindah ke direktori imagebuilder untuk Raspberry Pi 1
 cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-bcm27xx-bcm2708.Linux-x86_64 || exit
 
-# Hapus paket default yang tidak diperlukan
+# Remove redundant default packages
 sed -i "/luci-app-cpufreq/d" include/target.mk
 
-# Paksa opkg untuk menimpa file yang sudah ada
+# Force opkg to overwrite files
 sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
 
-# Jangan menghasilkan gambar ISO karena ukurannya terlalu besar
+# Not generate ISO images for it is too big
 sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
 
-# Jangan menghasilkan gambar VHDX
+# Not generate VHDX images
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
 
-# Tingkatkan ukuran partisi rootfs menjadi 1024M
+# Increase rootfs partition size to 1024M
 sed -i "s/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=1024/" .config
